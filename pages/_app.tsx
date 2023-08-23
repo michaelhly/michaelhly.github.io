84302160261256a05497f6ffb62e9f86ac6d1691
michaelhly <michaelhly@gmail.com>
2023-08-23 15:27:47 -0400

    Disable SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import "../styles/index.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
-}
+};
+
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+});
