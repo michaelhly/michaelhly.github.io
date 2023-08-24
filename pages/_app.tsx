bcfecbac8bc0ced33144dc57324aadf9fbd63c86
michaelhly <michaelhly@gmail.com>
2023-08-24 13:22:44 -0400

    Revert "Disable SSR"
    This reverts commit 84302160261256a05497f6ffb62e9f86ac6d1691.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
 import { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import "../styles/index.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-};
-
-export default dynamic(() => Promise.resolve(App), {
-  ssr: false,
-});
+}
