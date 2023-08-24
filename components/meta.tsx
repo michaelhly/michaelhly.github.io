36588d8f08a54ff5de18e6b37c9461c2803b9275
michaelhly <michaelhly@gmail.com>
2023-08-24 10:11:14 -0400

    Update favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,18 +1,19 @@
 import Head from "next/head";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
+import { HOME_OG_IMAGE_URL } from "../lib/constants";
 
 const Meta = () => {
   return (
     <Head>
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon/favicon.svg" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta name="description" content="Michael Huang's website" />
+      <meta property="og:title" content="Michael Huang" />
+      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
-    // <meta property="og:image" content={HOME_OG_IMAGE_URL} />
   );
 };
 
