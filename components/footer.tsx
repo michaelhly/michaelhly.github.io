629d91547726f2062a546273f17377b29b258301
michaelhly <michaelhly@gmail.com>
2023-08-24 16:29:05 -0400

    Swap fa logos

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="w-full bg-neutral-50 border-t border-neutral-200 fixed bottom-0">
       <Container>
         <div className="flex gap-4 py-2">
-          <a className="fas fa-envelope" href="https://github.com/michaelhly" />
-          <a className="fab fa-github" href="mailto:michaelhly@gmail.com" />
+          <a className="fab fa-github" href="https://github.com/michaelhly" />
+          <a className="fas fa-envelope" href="mailto:michaelhly@gmail.com" />
         </div>
       </Container>
     </footer>
