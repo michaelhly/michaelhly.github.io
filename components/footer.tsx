34e5c3a1c4bdae12e42558f1f00e8f0eb32ef2b7
michaelhly <michaelhly@gmail.com>
2023-08-25 20:44:11 -0400

    Add twitter link

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,9 @@ const Footer = () => {
     <footer className="w-full bg-neutral-50 border-t border-neutral-200 fixed bottom-0">
       <Container>
         <div className="flex gap-4 py-2">
-          <a className="fab fa-github" href="https://github.com/michaelhly" />
           <a className="fas fa-envelope" href="mailto:michaelhly@gmail.com" />
+          <a className="fab fa-twitter" href="https://twitter.com/michaelhly" />
+          <a className="fab fa-github" href="https://github.com/michaelhly" />
         </div>
       </Container>
     </footer>
